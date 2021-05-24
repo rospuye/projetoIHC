5ef6a895797dc9a39d9ed88129b79754e73a1857
rospuye <66674322+rospuye@users.noreply.github.com>
2021-05-24 16:34:50 +0100

    new wellness

--- WellnessCheck/wellnessCheck.js
+++ WellnessCheck/wellnessCheck.js
@@ -11,7 +11,8 @@ function validateWellnessCheck() {
     var q2a2 = document.getElementById("q2a2");
     var q2a3 = document.getElementById("q2a3");
     var q2a4 = document.getElementById("q2a4");
-    var boolean2 = q2a1.checked || q2a2.checked || q2a3.checked || q2a4.checked;
+    var q2a5 = document.getElementById("q2a5");
+    var boolean2 = q2a1.checked || q2a2.checked || q2a3.checked || q2a4.checked || q2a5.checked;
 
     var q3a1 = document.getElementById("q3a1");
     var q3a2 = document.getElementById("q3a2");
